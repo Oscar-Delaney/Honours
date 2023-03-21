7508fc9f94ac741adef475735f44ad1d6e1751ee
Oscar Delaney <o.delaney@uq.net.au>
2023-03-21 15:28:10 +1000

    I got a better version of an example ode working using a params matrix

--- deterministic.R
+++ deterministic.R
@@ -1,61 +1,77 @@
 library(deSolve)
 
-Psi = 0.1
-Phi = 0.2
-Zeta = 1
-Kappa = 1
+mutation_rate = 10^-1
 
 # Initialize 'params' matrix with 4 rows and 10 columns
+params <- matrix(nrow = 4, ncol = 10)
 
-fitness <- function(psi=Psi,A1,phi1=Phi,zeta1=Zeta,kappa1=Kappa,A2,phi2=Phi,zeta2=Zeta,kappa2=Kappa) {
-  return(psi-hill(psi,A1,phi1,zeta1,kappa1)-hill(psi,A2,phi2,zeta2,kappa2))
+# Name the rows of the matrix
+rownames(params) <- c("S", "R1", "R2", "R12")
+
+# Name the columns of the matrix
+colnames(params) <- c("psi", "phi1", "zeta1", "kappa1", "phi2", "zeta2", "kappa2", "mu", "k", "alpha")
+
+# Assign values to the 'psi' column of the matrix
+params[, "psi"] <- c(0.1, 0.1, 0.1, 0.1)
+
+# Assign values to the 'phi1' and 'phi2' columns of the matrix
+params[, c("phi1", "phi2")] <- c(0.2, 0.2, 0.2, 0.2)
+
+# Assign values to the 'zeta1' column of the matrix
+params[, "zeta1"] <- c(1, 100, 1, 100)
+
+# Assign values to the 'zeta2' column of the matrix
+params[, "zeta2"] <- c(1, 1, 100, 100)
+
+# Assign values to the 'kappa1' and 'kappa2' columns of the matrix
+params[, c("kappa1", "kappa2")] <- c(1, 1, 1, 1)
+
+# Assign values to the 'mu' column of the matrix
+params[, "mu"] <- c(1, 1, 1, 1)
+
+# Assign values to the 'k' column of the matrix
+params[, "k"] <- c(100, 100, 100, 100)
+
+# Assign values to the 'alpha' column of the matrix
+params[, "alpha"] <- c(1, 1, 1, 1)
+
+hill <- function(A, parms) {
+  psi = parms[1] ; phi = parms[2] ; zeta = parms[3]; kappa = parms[4]
+  return(phi*(A/zeta)^kappa/((A/zeta)^kappa-(psi-phi)/psi))
 }
 
-growth <- function(mu,R,k){
-  return(mu*R/(R+k))
+antibiotics <- function(A1,A2,parms) {
+  psi = parms[1]
+  return(psi-hill(A1,c(psi,parms[2:4]))-hill(A2,c(psi,parms[5:7])))
 }
 
-print(hill(0,2,1,1,1))
-print(fitness(A1=100,A2=1,zeta2=0.01))
+monod <- function(N,parms){
+  mu = parms[1] ; k = parms[2]
+  return(mu*N/(N+k))
+}
 
+deplete <- function(dS,dR,alpha=params["S","alpha"]) {
+  return(-alpha*(max(0,dS)+max(0,dR)))
+}
 
-# Function to simulate bacterial growth over time
-simulate_growth <- function(initial_populations, params, time) {
-  
-  # Define the differential equations for the model
-  bacterial_growth <- function(t, populations, params) {
-    with(list(S = populations[1], R = populations[2], rmax_S = params[1], rmax_R = params[2],
-              K = params[3], beta = params[4], delta = params[5]), {
-                dS <- rmax_S * S * (1 - ((S + R) / K)) - beta * S * R
-                dR <- rmax_R * R * (1 - ((S + R) / K)) + beta * S * R - delta * R
-                return(list(c(dS, dR)))
-              })
-  }
-  
-  # Set the initial populations and parameters
-  S0 <- initial_populations[1]
-  R0 <- initial_populations[2]
-  rmax_S <- params[1]
-  rmax_R <- params[2]
-  K <- params[3]
-  beta <- params[4]
-  delta <- params[5]
-  
-  # Set the times at which to compute the solutions
-  times <- seq(from = 0, to = time, by = 0.01)
-  
-  # Solve the differential equations using deSolve
-  sol <- ode(y = c(S = S0, R = R0), times = times, func = bacterial_growth, parms = params)
-  
-  # Return the solution
-  return(sol)
+# Define the differential equations for the model
+bacterial_growth <- function(t,populations,parms=params) {
+  with(as.list(c(populations)), {
+    S <- populations[1]
+    R <- populations[2]
+    N <- populations[3]
+    dS <- S*(monod(N,params["S",c("mu","k")]) + antibiotics(A1=1,A2=0,params["S",1:7]) - mutation_rate)
+    dR <- R*(monod(N,params["R1",c("mu","k")]) + antibiotics(A1=1,A2=0,params["R1",1:7])) + S*mutation_rate
+    dN <- deplete(dS,dR)
+    return(list(c(dS, dR, dN)))
+  })
 }
 
 # Example usage
-initial_populations <- c(S = 100, R = 10)
-params <- c(rmax_S = 0.8, rmax_R = 0.6, K = 1000, beta = 0.01, delta = 0.05)
-time <- 50
-solution <- simulate_growth(initial_populations, params, time)
-plot(solution[,1], solution[,3], type = "l", col = 2, main = "Bacterial growth over time", xlab = "Time", ylab = "Population size")
-lines(solution[,1], solution[,2], col = 1)
-legend("topright", legend = c("Susceptible", "Resistant"), col = c(1, 2), lty = 1)
+times <- seq(0,50,0.01)
+solution <- ode(y = c(S = 100, R = 10, N = 1000), times = times, func = bacterial_growth)
+plot(solution[, 1], solution[,2],type = "l", col = 1, main = "Bacterial growth over time", xlab = "Time", ylab = "Population size")
+lines(solution[, 1],solution[, 3], col = 2)
+lines(solution[, 1],solution[, 4], col = 3)
+legend("topright", legend = c("Susceptible", "Resistant", "Nutrient"), col = c(1, 2, 3), lty = 1)
+
