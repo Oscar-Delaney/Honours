d2f8daf7b2417d24b6443195fa9b956fc37793b9
Oscar Delaney <o.delaney@uq.net.au>
2023-03-22 18:44:02 +1000

    Not currently working (?)

--- deterministic.R
+++ deterministic.R
@@ -1,9 +1,21 @@
 library(deSolve)
 
-D = 0.1 #bottleneck dilution ratio
-N0 = 100 #initial nutrient concentration
-m1 = 0.1 #rate of mutations conferring resistance to drug 1
-m2 = 0.1 #rate of mutations conferring resistance to drug 2
+pharmacokinetic <- FALSE # should be either TRUE or FALSE
+stewardship <- "comb" # "cycl" or "comb" or "1_only" or "2_only"
+
+# Define the parameters of the model
+D <- 0.1 # bottleneck dilution ratio
+N0 <- 100 # initial nutrient concentration
+m1 <- 0.1 # rate of mutations conferring resistance to drug 1
+m2 <- 0.1 # rate of mutations conferring resistance to drug 2
+d1 <- 1*pharmacokinetic # rate of drug 1 elimination
+d2 <- 1*pharmacokinetic # rate of drug 2 elimination
+i1 <- 10 # drug 1 influx concentration
+i2 <- 10 # drug 2 influx concentration
+if (stewardship=="2_only") {pattern <- c(0,1)
+  } else if (stewardship=="comb") {pattern <- c(1,1)
+  } else {pattern <- c(1,0)
+}
 
 # Initialize 'params' matrix with 4 rows and 10 columns
 params <- matrix(nrow = 4, ncol = 12)
@@ -68,8 +80,10 @@ deplete <- function(dS=0,dR1=0,dR2=0,dR12=0,alpha=params[,"alpha"]) {
 
 # Define a bottleneck function that reduces all populations by a fixed fraction
 bottleneck <- function(t, populations, parms = params) {
-  populations <- c(populations[1:4]*D, N0)
-  populations[populations < 0] <- 0
+  if (stewardship=="cycl") {pattern <<- 1 - pattern}
+  populations <- c(populations[1:4]*D, N0, pharmacokinetic*populations[6:7]+c(i1,i2)*pattern)
+  # names(populations) <- c("S", "R1", "R2", "R12", "N", "A1", "A2")
+  populations[populations < 0] <- 0 # backup - shouldn't be needed
   return(populations)
   }
 
@@ -82,8 +96,10 @@ bacteria_plot <- function(solution) {
   lines(solution[, 1],solution[, 4], col = 3)
   lines(solution[, 1],solution[, 5], col = 4)
   lines(solution[, 1],solution[, 6], col = 5)
-  legend("topright", legend = c("S", "R1", "R2", "R12", "Nutrient"),
-  col = c(1, 2, 3, 4, 5), lty = 1)
+  lines(solution[, 1],solution[, 7], col = 6)
+  lines(solution[, 1],solution[, 8], col = 7)
+  legend("topright", legend = c("S", "R1", "R2", "R12", "Nutrient","A1","A2"),
+    col = c(1, 2, 3, 4, 5, 6, 7), lty = 1)
 }
 
 
@@ -95,31 +111,36 @@ bacterial_growth <- function(t, populations, parms = params) {
     R2 <- populations[3]
     R12 <- populations[4]
     N <- populations[5]
-    dS <- S*((1-m1)*(1-m2)*monod(N,params["S",8:9]) - 
-      hill(A=1,params["S",1:4]) - 
-      hill(A=0,params["S",c(1,5:7)]))
-    dR1 <- R1*((1-m2)*monod(N,params["R1",8:9]) - 
-      hill(A=1,params["R1",1:4]) - 
-      hill(A=0,params["R1",c(1,5:7)])) +
+    A1 <- populations[6]
+    A2 <- populations[7]
+    dS <- S*((1-m1)*(1-m2)*monod(N,params["S",8:9]) -
+      hill(A=A1,params["S",1:4]) - 
+      hill(A=A2,params["S",c(1,5:7)]))
+    dR1 <- R1*((1-m2)*monod(N,params["R1",8:9]) -
+      hill(A=A1,params["R1",1:4]) - 
+      hill(A=A2,params["R1",c(1,5:7)])) +
       S*m1*(1-m2)*monod(N,params["S",8:9])
-    dR2 <- R2*((1-m1)*monod(N,params["R2",8:9]) - 
-      hill(A=1,params["R2",1:4]) - 
-      hill(A=0,params["R2",c(1,5:7)])) +
+    dR2 <- R2*((1-m1)*monod(N,params["R2",8:9]) -
+      hill(A=A1,params["R2",1:4]) - 
+      hill(A=A2,params["R2",c(1,5:7)])) +
       S*(1-m1)*m2*monod(N,params["S",8:9])
     dR12 <- R12*(monod(N,params["R12",8:9]) -
-      hill(A=1,params["R12",1:4]) - 
-      hill(A=0,params["R12",c(1,5:7)])) +
+      hill(A=A1,params["R12",1:4]) - 
+      hill(A=A2,params["R12",c(1,5:7)])) +
       S*m1*m2*monod(N,params["S",8:9]) +
       R1*m2*monod(N,params["R1",8:9]) +
       R2*m1*monod(N,params["R2",8:9])
     dN <- deplete(dS,dR1,dR2,dR12,params[,"alpha"])
-    return(list(c(dS, dR1, dR2, dR12, dN)))
+    dA1 <- -d1
+    dA2 <- -d2
+    return(list(c(dS, dR1, dR2, dR12, dN, dA1, dA2)))
   })
 }
 
 # Example usage
 times <- seq(0,50,0.01)
-populations <- c(S = 100, R1 = 1, R2 = 1, R12 = 0, N = 100)
+populations <- c(S = 100, R1 = 10, R2 = 10, R12 = 0, N = 100,
+  A1 = i1*pattern[1], A2 = i2*pattern[2])
 events <- list(func = bottleneck, times = seq(10,40,10))
 solution <- ode(y = populations, times = times, func = bacterial_growth, events = events)
 bacteria_plot(solution)
