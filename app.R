0f1efe2a241962c3641839d720c58be6e959a7a5
Oscar Delaney <o.delaney@uq.net.au>
2023-05-18 16:08:14 +1000

    updated shiny app (separate b'neck and dosing)

--- app.R
+++ app.R
@@ -27,6 +27,12 @@ rate with unlimited nutrients. K is the nutrient concentration that produces
 half the maximal growth rate. Alpha is the amount of nutrients used per new
 bacterial cell."
 
+events_text <- "The two possible time-discontinuous features of the model
+are dosing with antibiotics, and bottlenecks where the population size is
+reduced proportionally and new nutrients are added. Drugs can be set to
+disappear when the next dose arrives, which is less realistic but sometimes
+convenient, or to persist through dosing."
+
 pd_text <- "Phi_i is the maximum reduction in growth rate caused by
 antibiotic i. Zeta_i is the concentration of antibiotic i resulting in half
 the maximal death rate. Kappa_i is the shape parameter, with smaller values
@@ -34,14 +40,18 @@ representing a steep initial increase in death rate, and shallow gradient about
 zeta_i. Theta is the drug-drug interaction parameter, in the range [-1,1]."
 
 # Default values
-drugs_default <- matrix(c(
-    1e-9, 1e-9, # mutation rates
-    rep(round(log(2) / 3.5, 3), 2), # drug elimination rates
-    7, 7 # drug influx concentrations, MIC units
-), nrow = 2, ncol = 3, dimnames = list(
-    c("A1", "A2"),
-    c("Mutation rate", "Elimination rate", "Influx")
-))
+drugs_default <- matrix(
+    c(
+        1e-9, 1e-9, # mutation rates
+        rep(round(log(2) / 3.5, 3), 2), # drug elimination rates
+        7, 7 # drug influx concentrations, MIC units
+    ),
+    nrow = 2, ncol = 3,
+    dimnames = list(
+        c("A1", "A2"),
+        c("Mutation rate", "Elimination rate", "Influx")
+    )
+)
 
 growth_default <- matrix(
     c(
@@ -74,6 +84,54 @@ pd_default <- matrix(
     )
 )
 
+basic_content <- wellPanel(
+    p(basic_text),
+    numericInput("rep", "Number of Runs", value = 1, min = 1, step = 1),
+    numericInput("time", "Simulation Time (hours)", value = 100, step = 1),
+    numericInput("dt", "Granularity (hours)", value = 0.1, step = 0.1),
+    checkboxInput("deterministic", "Deterministic Model", FALSE),
+    checkboxInput("cycl", "Cycle between drugs", TRUE),
+    numericInput("seed", "Random Seed", value = NULL)
+)
+
+drugs_content <- wellPanel(
+    p(drugs_text),
+    matrixInput("drugs", value = drugs_default, class = "numeric"),
+    numericInput("HGT", "recombination rate", value = 0, min = 0, step = 1e-15)
+)
+
+growth_content <- wellPanel(
+    p(growth_text),
+    matrixInput("growth", value = growth_default, class = "numeric")
+)
+
+events_content <- wellPanel(
+    p(events_text),
+    numericInput("tau", "Bottleneck frequency (hours)", value = 10, step = 1),
+    numericInput("N0", "Bottleneck Nutrient pulse", value = "1e15", step = 1e9),
+    numericInput("D", "Bottleneck Dilution fraction", value = 1e-1, step = 0.1),
+    numericInput("dose_gap", "Gap between doses (hours)", value = 10, step = 1),
+    numericInput("dose_rep", "Drug cycling period (doses)", value = 1, step = 1),
+    checkboxInput("keep_old_drugs", "Old drugs persist through dosing", TRUE),
+)
+
+pd_content <- wellPanel(
+    p(pd_text),
+    matrixInput("pd", value = pd_default, class = "numeric")
+)
+
+graph_content <- tagList(
+    radioButtons("display", "Uncertainty Display",
+        choices = c(
+            "Median + 25th and 75th percentiles" = "median",
+            "Mean + 95% confidence interval" = "mean",
+            "Plot all the runs" = "all"
+        ),
+        selected = "all"
+    ),
+    plotOutput("plot", height = "800", width = "100%")
+)
+
 # UI
 ui <- fluidPage(
     useShinyjs(),
@@ -86,99 +144,12 @@ ui <- fluidPage(
         id = "everything",
         mainPanel(
             tabsetPanel(
-                tabPanel(
-                    "Basic",
-                    fluidRow(
-                        column(6, wellPanel(
-                            p(basic_text),
-                            numericInput("rep", "Number of Runs", value = 1, min = 1, step = 1),
-                            numericInput("time", "Simulation Time (hours)", value = 100, step = 1),
-                            checkboxInput("deterministic", "Deterministic Model", FALSE),
-                            numericInput("seed", "Random Seed", value = NULL),
-                            checkboxInput("cycl", "Cycle between drugs", TRUE),
-                            numericInput("dose_rep", "Number of doses before switching drugs",
-                              value = 1, min = 1, step = 1)
-                            # sliderInput("dt", "log_10 of time between data points (hours)",
-                            #     value = -1, min = -3, max = 0, step = 0.1
-                            # )
-                            )
-                        )
-                    )
-                ),
-                tabPanel(
-                    "Drugs",
-                    fluidRow(
-                        column(6, wellPanel(
-                            p(drugs_text),
-                            matrixInput("drugs",
-                                value = drugs_default,
-                                rows = list(extend = FALSE, names = TRUE),
-                                cols = list(extend = FALSE, names = TRUE),
-                                class = "numeric"
-                            ),
-                            numericInput("HGT", "recombination rate",
-                                value = 0, min = 0, max = 1, step = 1e-15
-                            )
-                        ))
-                    )
-                ),
-                tabPanel(
-                    "Growth",
-                    fluidRow(
-                        column(6, wellPanel(
-                            p(growth_text),
-                            matrixInput("growth",
-                                value = growth_default,
-                                rows = list(extend = FALSE, names = TRUE),
-                                cols = list(extend = FALSE, names = TRUE),
-                                class = "numeric"
-                            )
-                        )),
-                    )
-                ),
-                tabPanel(
-                    "Bottlenecks",
-                    fluidRow(
-                        column(6, wellPanel(
-                            sliderInput("freq", "Bottleneck Frequency (hours)",
-                                value = 10, min = 1, max = 30, step = 1
-                            ),
-                            sliderInput("N0", "log_10 of Nutrient influx at bottleneck",
-                                value = 15, min = 2, max = 20, step = 0.1
-                            ),
-                            sliderInput("D", "log_10 of Dilution fraction at bottleneck",
-                                value = -1, min = -5, max = 0, step = 0.1
-                            )
-                        ))
-                    )
-                ),
-                tabPanel(
-                    "Pharmacodynamics",
-                    fluidRow(
-                        column(12, wellPanel(
-                            p(pd_text),
-                            matrixInput("pd",
-                                value = pd_default,
-                                rows = list(extend = FALSE, names = TRUE),
-                                cols = list(extend = FALSE, names = TRUE),
-                                class = "numeric"
-                            )
-                        ))
-                    )
-                ),
-                tabPanel(
-                    "Graph",
-                    radioButtons("display", "Uncertainty Display",
-                        choices = c(
-                            "Median + 25th and 75th percentiles" = "median",
-                            "Mean + 95% confidence interval" = "mean",
-                            "Plot all the runs" = "all"
-                        ),
-                        selected = "all"
-                    ),
-                    plotOutput("simulation_plot", height = "800", width = "100%")
-                )
-                # Add other tabs here
+                tabPanel("Basic", fluidRow(column(6, basic_content))),
+                tabPanel("Drugs", fluidRow(column(6, drugs_content))),
+                tabPanel("Growth", fluidRow(column(6, growth_content))),
+                tabPanel("Events", fluidRow(column(6, events_content))),
+                tabPanel("Pharmacodynamics", fluidRow(column(12, pd_content))),
+                tabPanel("Graph", graph_content)
             )
         )
     )
@@ -193,11 +164,13 @@ server <- function(input, output, session) {
             seed = input$seed,
             cycl = input$cycl,
             dose_rep = input$dose_rep,
+            dose_gap = input$dose_gap,
+            keep_old_drugs = input$keep_old_drugs,
             time = input$time,
-            freq = input$freq,
-            # dt = 10^input$dt,
-            N0 = round(10^input$N0),
-            D = 10^input$D,
+            tau = input$tau,
+            dt = input$dt,
+            N0 = input$N0,
+            D = input$D,
             HGT = input$HGT,
             m1 = input$drugs["A1", "Mutation rate"],
             m2 = input$drugs["A2", "Mutation rate"],
@@ -217,12 +190,11 @@ server <- function(input, output, session) {
             alpha = input$growth[, "Alpha"]
         )
     })
-
-    output$simulation_plot <- renderPlot({
+    output$plot <- renderPlot({
         # Check if the simulation_result has been executed
         if (!is.null(simulation_result())) {
             # Create a plot of the simulation results
-            log_plot(simulation_result(), type = input$display)
+            log_plot(simulation_result()[[1]], type = input$display)
         }
     })
     # add code to reset all parameters to defaults
