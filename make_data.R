b7bfa68c8efee8a8c795fa675b6032edf433380f
Oscar Delaney <o.delaney@uq.net.au>
2023-06-02 17:00:53 +1000

    dose rep and hill shape parameter etc

--- make_data.R
+++ make_data.R
@@ -37,33 +37,38 @@ metrics_plot <- function(summary, metric) {
     )
 }
 
+log_plot(data[[72]][[1]][data[[1]][[1]]$rep <= 10, ])
+sol <- data[[3]][[1]]
+sol[sol$variable == "N" & sol$time == max(sol$time), ]$value
 # Create a grid of parameter combinations
-time <- 5e2
-# freq_range <- seq(1, 30, by = 1)
-# D_range <- 10 ^ (-0.1 * freq_range)
-HGT_range <- 10 ^ seq(-20, -10, by = 5)
-summary <- expand.grid(HGT = HGT_range)
-# summary <- data.frame(dose_rep = 0:0)
-summary$wins <- 0
+time <- 3e2
+# summary <- expand.grid(HGT = HGT_range)
+summary <- expand.grid(dose_rep = seq(1, 24, 3), kappa = seq(0.5, 3, 0.3))
+# summary$wins <- 0
 
-# Run the simulations (case 1)
 data <- list()
 for (i in seq_len(nrow(summary))) {
-    data[[i]] <- 
+    data[[i]] <- simulate(
+    init = c(S = 1e15, R1 = 0, R2 = 0, R12 = 0),
+    N0 = 1e16,
+    k = 0,
+    alpha = 0,
+    mu = 0.15 * c(1, 0.8, 0.8, 0.64),
+    phi1 = 0.6,
+    phi2 = 0.6,
+    time = time,
+    rep = 1e2,
+    D = 1,
+    HGT = 0,
+    dose_rep = summary$dose_rep[i],
+    dose_gap = 24 / summary$dose_rep[i],
+    influx = 15 * c(A1 = 1, A2 = 1) / summary$dose_rep[i],
+    m1 = 1.5e-8, m2 = 1.5e-8,
+    kappa1 = summary$kappa[i],
+    kappa2 = summary$kappa[i],
+)
 print(i / nrow(summary))
 }
-log_plot(sol <- simulate(
-    init = c(S = 3e7, R1 = 0, R2 = 0, R12 = 0),
-    N0 = 1e12,
-    k = 1e10,
-    mu = 1.5 * c(1, 0.9, 0.9, 0.81),
-    phi1 = 2,
-    phi2 = 2,
-    time = 60,
-    rep = 10,
-    D = 1,
-    HGT = 0
-)[[1]])
 
 # Calculate the summary statistics
 for (i in seq_len(nrow(summary))) {
@@ -76,30 +81,6 @@ for (i in seq_len(nrow(summary))) {
     summary$ymax[i] <- ci[2]
 }
 
-# Run the simulations (case 2)
-data <- list()
-for (i in seq_len(nrow(summary))) {
-    HGT <- summary$HGT[i]
-    data[[i]] <- simulate(
-        seed = NULL,
-        init = 1e10 * c(S = 1, R1=0, R2=0, R12=0),
-        N0 = 1e11,
-        k = 1e10,
-        mu = 0.6 * c(1, 1, 1, 0.5),
-        time = 500,
-        rep = 100,
-        HGT = HGT
-    )
-}
-
-# Calculate the summary statistics
-for (i in seq_len(nrow(summary))) {
-    sols <- data[[i]][[1]]
-    summary$final_pop[i] <- 4 * mean(sols$value[sols$time >= 400])
-}
-
-# add error bars
-png("test.png", width = 12, height = 10, units = "in", res = 300)
 # plot with one independent variable and one dependent variable
 ggplot(summary, aes(x = dose_rep, y = wins)) +
     geom_point(size = 3) +
@@ -108,16 +89,17 @@ ggplot(summary, aes(x = dose_rep, y = wins)) +
     # scale_x_log10() +
     scale_y_continuous(limits = c(0,1)) +
     labs(
-        title = "Cycling in strong-selection weak-mutation regime",
-        x = "Doses per cycle (0 = combination therapy)",
-        y = "Probability that MDR emerges in 1000 hours"
+        title = "Resistance Evolution",
+        x = "Number of doses per day",
+        y = "Probability that MDR emerges in 300 hours"
     ) +
     theme(
         plot.title = element_text(size = 32, face = "bold", hjust = 0.5),
         axis.title = element_text(size = 25, face = "bold"),
         axis.text = element_text(size = 25)
     )
-dev.off()
+
+
 # plot with two independent variables and one dependent variable
 ggplot(summary, aes(x = D, y = HGT, size = wins)) +
     geom_point() +
@@ -140,10 +122,6 @@ ggplot(summary, aes(x = D, y = HGT, size = wins)) +
     )
 
 # Next I will make a plot similar to that above, but with a colour scale
-# rather than size to represent the probability of MDR emergence
-# how could I make the color scale in the legend bigger?
-# answer: use legend.key.size
-# make the graph
 ggplot(summary, aes(x = D, y = HGT, color = wins)) +
     geom_point(size = 10) +
     scale_x_log10() +
@@ -165,53 +143,24 @@ ggplot(summary, aes(x = D, y = HGT, color = wins)) +
         legend.key.size = unit(2, "cm")
     )
 
+ggplot(summary, aes(x = dose_rep, y = kappa)) +
+    geom_tile(aes(fill = wins)) +
+    scale_fill_gradient(low = "white", high = "blue", limits = c(0, 1)) +
+    labs(x = "doses per day", y = "kappa", fill = "proportion",
+            title = "Hill shape parameter interacts with dosing frequency",
+            subtitle = "proportion of runs where MDR becomes established") +
+    theme_minimal() +
+    theme(
+        plot.title = element_text(size = 29, face = "bold", hjust = 0.5),
+        plot.subtitle = element_text(size = 25, hjust = 0.5),
+        axis.title = element_text(size = 25, face = "bold"),
+        axis.text = element_text(size = 25),
+        legend.title = element_text(size = 20),
+        legend.text = element_text(size = 20)
+    )
+
+
 # # Save the simulation results to a file
 save(data, file = "C:/Users/s4528540/Downloads/results/data4.RData")
 # read the data file we just wrote back as data
 load("C:/Users/s4528540/Downloads/results/data4.RData")
-
-# Plot the summary statistics
-metrics_plot(summary, "R12")
-metrics_plot(summary, "total")
-log_plot(data[[97]][[1]], type = "all")
-
-
-# Trying to find regions of paramter space where cycling is best
-log_plot(simulate(
-      rep = 1, dose_rep = 10, init = c(S = 1e8, R1=0, R2=0, R12=0),
-      time = 300, D = 1, HGT = 0, freq = 10, N0 = 1e9, theta = 0,
-      d1 = 0, d2 = 0, m1 = 5e-9, m2 = 5e-9, phi1 = 1, phi2 = 1,
-      zeta1 = c(S = 1,8,4,8), zeta2 = c(S = 1,4,8,8),
-      mu = c(1, 0.9, 0.9, 0.81), alpha = 0, k = 1e7, cycl = F)[[1]],
-      type = "all")
-
-
-
-# Weak mutation strong selection recombination is less useful
-time <- 30
-init_S <- 1e6
-df <- data.frame(max_step = 10 ^ seq(0, -4, by = -1), run_time = NA, r_error = NA)
-
-for (i in seq_len(nrow(df))) {
-    df$run_time[i] <- system.time({
-        sol = simulate(
-            init = c(S = init_S, R1=0, R2=0, R12=0),
-            tau = 1e3,
-            mu = 1,
-            alpha = 0,
-            k = 0,
-            time = time,
-            dose_rep = 1,
-            rep = 1,
-            influx = c(A1 = 0, A2 = 0),
-            HGT = 0,
-            m1 = 0,
-            m2 = 0,
-            max_step = df$max_step[i],
-            deterministic = F
-            )[[1]]
-    })["elapsed"]
-    df$r_error[i] <- 1 - log(sol[sol$time == time & sol$variable == "S",]$value / init_S) / time
-    print(i/nrow(df))
-}
-df
