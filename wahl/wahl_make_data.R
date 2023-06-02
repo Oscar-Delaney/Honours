dbf9a80961715d1e4ccf09fff27a2e743755a4e4
Oscar Delaney <o.delaney@uq.net.au>
2023-06-02 17:01:48 +1000

    redone metric (rate rather than binary) added theory component resource use analysis

--- wahl/wahl_make_data.R
+++ wahl/wahl_make_data.R
@@ -1,31 +1,118 @@
 # Critique of Wahl papers
-source("wahl_code.R")
+source("wahl/wahl_code.R")
+library(survival)
 
-log_plot(data3[[11]][[1]][data1[[1]][[1]]$rep >= 990, ], type = "all")
+# calculate the theoretical solution
+solve_phi <- Vectorize(
+    function(D, r, s) {
+        # Define the function whose root we want to find
+        f <- function(phi) {
+            return(phi - (D * phi + (1 - D)) ^ (D ^ -(1 + s)))
+        }
+        # Use uniroot to find the root
+        phi <- uniroot(f, lower = 0, upper = 1-1e-10)
+        return(phi$root)
+    }
+)
+
+# theoretical rate at which beneficial mutations fix
+fix_rate <- function(D, r, s, N, mu) {
+    phi <- solve_phi(D, r, s)
+    theta <- D * N * mu * ((D ^ -s) - 1) / s
+    tau <- -log(D) / r
+    return(theta * (1 - phi) / tau)
+}
+
+# evaluate a set of simulation results
+metric <- function(summary, data, threshold = 1e2) {
+    for (i in seq_len(nrow(summary))) {
+        # define the minimum population size for a mutant to be ~fixed
+        c <- threshold / data[[i]][[2]]$D
+        dt <- data[[i]][[2]]$dt # time step
+        # find the time at which the mutant first appears en route to fixation
+        survival <- data[[i]][[1]] %>%
+            group_by(rep) %>%
+            summarise(
+                fix = any(value[variable == "M"] > c),
+                t = max(time[variable == "M" & near(value, 0)]) + dt
+            )
+
+        # fit the model
+        exp_fit <- survreg(Surv(t, fix) ~ 1, data = survival, dist = "exp")
+
+        # extract the log-scale estimate and standard error
+        log_estimate <- exp_fit$coefficient[1]
+        log_se <- sqrt(exp_fit$var)[1]
+
+        # compute a 95% confidence interval on the log scale
+        log_ci <- log_estimate + log_se * qnorm(c(0.025, 0.975))
+
+        # exponentiate to get a confidence interval for the mean on the original scale
+        ci <- exp(log_ci)
+
+        # store in summary
+        summary$time[i] <- exp(log_estimate)
+        summary$ci_lower[i] <- ci[1]
+        summary$ci_upper[i] <- ci[2]
+        summary$naive_mean[i] <- mean(survival$t)
+
+        print(i / nrow(summary))
+    }
+    return(summary)
+}
+
+# check if theta theory matches practice
+sols <- simulate(
+    seed = NULL,
+    rep = 1e4,
+    time = 2.01,
+    dt = 1e-2,
+    tau = 2,
+    D = exp(-2),
+    N0 = 1e9,
+    k = 0,
+    alpha = 0,
+    mu = c(W = 1.024, M = 1.024 * (1 + 0.1)),
+    init = c(W = round(1e9 * exp(-2)), M = 0),
+    m1 = 1e-9
+)[[1]]
+sum(sols$value[sols$variable == "M" & sols$time == 2.01]) / 1e4
+D = exp(-2)
+D/s * (D^-s -1)
+solve_phi(0.9, 1, s)
+log_plot(sols)
+summary <- metric(summary, data)
+summary$theory <- fix_rate(summary$D, 1, s, N0, 1e-9)
+summary
+log_plot(data[[1]][[1]][data[[1]][[1]]$rep <= 10 & data[[1]][[1]]$time < 300, ])
+
+log_plot(simulate(deterministic = F, time = 100, mu = 1.024, dt = 1e-3, tau = 0.1, D = exp(-0.1), k=0, alpha = 0)[[1]])
 
 s <- 0.1
-N0 <- 3e8
-time <- 100
+N0 <- 1e9
+time <- 50
 
 # Wahl 1 No resource constraints
-r <- 1.03
-summary <- data.frame(D = 10 ^ - seq(0.05, 4, by = 0.2))
-data1 <- list()
+r <- 1.024
+summary <- data.frame(D = 10 ^ - seq(0.1, 2, by = 0.2))
+data <- list()
 for (i in seq_len(nrow(summary))) {
     D <- summary$D[i]
-    data1[[i]] <- simulate(
+    data[[i]] <- simulate(
         seed = NULL,
-        rep = 1e0,
+        rep = 1e3,
         time = time,
+        dt = 1e-2,
         tau = - log(D),
         D = D,
         N0 = N0,
-        k = 1e-2 * N0,
+        k = 0,
         alpha = 0,
         mu = c(W = r, M = r * (1 + s)),
-        init = c(W = round(N0 * D), M = 0)
+        init = c(W = round(N0 * D), M = 1),
+        m1 = 0
     )
-    print(i/nrow(summary))
+    print(i / nrow(summary))
 }
 
 
@@ -78,34 +165,64 @@ for (i in seq_len(nrow(summary))) {
 summary$wins <- 0
 for (i in seq_len(nrow(summary))) {
     # count the number of runs on which M was at least 1e-5 of W at the end
-    end <- data3[[i]][[1]][data3[[i]][[1]]$time == time, ]
+    end <- data[[i]][[1]][data[[i]][[1]]$time == time, ]
     M <- end[end$variable == "M",]$value
     W <- end[end$variable == "W",]$value
-    summary$wins[i] <- mean(ifelse(W==0, M, M / W) > 1e3 / N0)
-    rep <- data3[[i]][[2]]$rep
+    summary$wins[i] <- mean(ifelse(W==0, M, M / W) > 1e2 / N0)
+    rep <- data[[i]][[2]]$rep
     ci <- binom.test(summary$wins[i] * rep, rep, conf.level = 0.95)$conf.int
-    summary$ymin[i] <- ci[1]
-    summary$ymax[i] <- ci[2]
+    summary$ci_lower[i] <- ci[1]
+    summary$ci_upper[i] <- ci[2]
 }
 
-png("wahl3.png", width = 12, height = 10, units = "in", res = 300)
-ggplot(summary, aes(x = D, y = wins)) +
+summary$phi <- solve_phi(summary$D, 1, s)
+summary$phi <- 1 + 2*s*log(summary$D)
+
+# png("wahl3.png", width = 12, height = 10, units = "in", res = 300)
+ggplot(summary, aes(x = D, y = time)) +
     geom_point(size = 3) +
-    geom_errorbar(aes(ymin = ymin, ymax = ymax)) +
+    geom_errorbar(aes(ymin = ci_lower, ymax = ci_upper)) +
+    geom_line(aes(y = 1 / theory), color = "red") +
     theme_light() +
     scale_x_log10() +
-    scale_y_continuous(limits = c(0,1)) +
+    scale_y_log10() +
+    # scale_y_continuous(limits = c(0,1)) +
     labs(
-        title = "Optimal Dilution Ratio (constant resource flux)",
+        title = "Optimal Dilution Ratio (resource unconstrained)",
         x = "D",
-        y = "Probability that a mutant fixes in 100 hours"
+        y = "Hours per beneficial mutation that goes on to fix",
+        color = "Theory"
     ) +
     theme(
         plot.title = element_text(size = 26, face = "bold", hjust = 0.5),
         axis.title = element_text(size = 25, face = "bold"),
         axis.text = element_text(size = 25)
     )
-dev.off()
+
+ggplot(summary, aes(x = D, y = wins)) +
+    geom_point(size = 3) +
+    geom_errorbar(aes(ymin = ci_lower, ymax = ci_upper)) +
+    geom_line(aes(y = 1 - phi, color = "theory")) +
+    theme_light() +
+    scale_x_log10() +
+    # scale_y_log10() +
+    scale_y_continuous(limits = c(0,1)) +
+    scale_color_manual(values = c("theory" = "red")) +
+    labs(
+        title = "Optimal Dilution Ratio (resource unconstrained)",
+        x = "D",
+        y = "1 - phi: probability of a single mutant at t = 0 eventually fixing",
+        color = NULL
+    ) +
+    theme(
+        plot.title = element_text(size = 26, face = "bold", hjust = 0.5),
+        axis.title = element_text(size = 25, face = "bold"),
+        axis.text = element_text(size = 25),
+        legend.title = element_text(size = 20),
+        legend.text = element_text(size = 20),
+        legend.position = "bottom"
+    )
+# dev.off()
 
 data5 <- list()
 r <- 1.2
@@ -216,3 +333,8 @@ ggplot(summary, aes(x = D, y = tau)) +
         legend.title = element_text(size = 20),
         legend.text = element_text(size = 20)
     )
+
+# unused
+survival$rexp <- sort(rexp(100, 1 / summary$time[1]))
+plot(survival$rexp, sort(survival$t), log = "xy", xlim = c(1e-2, 5e1), ylim = c(1e-2, 5e1))
+abline(0, 1)
