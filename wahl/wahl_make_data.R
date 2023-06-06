4a19cf9e985c969945f2fe38db8f7473cf513e6a
Oscar Delaney <o.delaney@uq.net.au>
2023-06-06 16:19:56 +1000

    many changes, sorry poorly documented just trying things out, will make the code nice later

--- wahl/wahl_make_data.R
+++ wahl/wahl_make_data.R
@@ -1,10 +1,11 @@
 # Critique of Wahl papers
 source("wahl/wahl_code.R")
 library(survival)
+library(hypergeo)
 
 # calculate the theoretical solution
 solve_phi <- Vectorize(
-    function(D, r, s) {
+    function(D, s) {
         # Define the function whose root we want to find
         f <- function(phi) {
             return(phi - (D * phi + (1 - D)) ^ (D ^ -(1 + s)))
@@ -17,17 +18,29 @@ solve_phi <- Vectorize(
 
 # theoretical rate at which beneficial mutations fix
 fix_rate <- function(D, r, s, N, mu) {
-    phi <- solve_phi(D, r, s)
-    theta <- D * N * mu * ((D ^ -s) - 1) / s
-    tau <- -log(D) / r
-    return(theta * (1 - phi) / tau)
+    return(ifelse(D == 1, s / (s + 1), 
+    (-N * mu * r * (D ^ -s - 1) ^ 2) /
+    (s * log(D) * (D ^ -(s + 1) - 1))))
 }
 
-# new fix rate
-fix_rate2 <- function(D, r, s, N, mu) {
-    return(ifelse(D == 1, s / (s + 1), 
-    (-N * mu * r * (1 - D ^ -s) ^ 2) /
-    (s * log(D) * (D ^ (s - 1) - D ^ (2 * s)))))
+# Hypergeometric function
+hyper <- function(s, z) {
+    return(Re(hypergeo(1, 1 / (1 + s), 1 + 1 / (1 + s), z)))
+}
+
+# theoretical rate function
+theory <- function(D, r, s, N, mu) {
+    return(-r * N * mu / log(D) * (hyper(s,(1 - D) / (D * (D ^ s - 1))) -
+        D * hyper(s, (1 - D) * (D ^ s) /  (D ^ s - 1))))
+}
+
+
+phi <- function(D, s) {
+    return(ifelse(D == 1, 1 / (1 + s), (1 - D) / (D ^ -s - D)))
+}
+
+theta <- function(D, s) {
+    return(D * (D ^ -s - 1) / s)
 }
 
 # evaluate a set of simulation results
@@ -36,12 +49,15 @@ metric <- function(summary, data, threshold = 1e2) {
         # define the minimum population size for a mutant to be ~fixed
         c <- threshold / data[[i]][[2]]$D
         dt <- data[[i]][[2]]$dt # time step
+        max_pop <- data[[i]][[2]]$init["W"] / data[[i]][[2]]$D
+        mut_rate <- data[[i]][[2]]$m1
+        mut_per_gen <- 1 # mut_rate * max_pop
         # find the time at which the mutant first appears en route to fixation
         survival <- data[[i]][[1]] %>%
             group_by(rep) %>%
             summarise(
                 fix = any(value[variable == "M"] > c),
-                t = max(time[variable == "M" & near(value, 0)]) + dt
+                t = max(time[variable == "M" & near(value, 0)], 0) + dt
             )
 
         # fit the model
@@ -58,53 +74,37 @@ metric <- function(summary, data, threshold = 1e2) {
         ci <- exp(log_ci)
 
         # store in summary
-        summary$time[i] <- exp(log_estimate)
-        summary$ci_lower[i] <- ci[1]
-        summary$ci_upper[i] <- ci[2]
-        summary$naive_mean[i] <- mean(survival$t)
+        summary$rate[i] <- 1 / exp(log_estimate) / mut_per_gen
+        summary$ci_lower[i] <- 1 / ci[2] / mut_per_gen
+        summary$ci_upper[i] <- 1 / ci[1] / mut_per_gen
+        summary$naive_mean[i] <- 1 / mean(survival$t) / mut_per_gen
+        summary$wins[i] <- mean(survival$fix)
 
         print(i / nrow(summary))
     }
     return(summary)
 }
 
-# check if theta theory matches practice
-sols <- simulate(
-    seed = NULL,
-    rep = 1e4,
-    time = 2.01,
-    dt = 1e-2,
-    tau = 2,
-    D = exp(-2),
-    N0 = 1e9,
-    k = 0,
-    alpha = 0,
-    mu = c(W = 1.024, M = 1.024 * (1 + 0.1)),
-    init = c(W = round(1e9 * exp(-2)), M = 0),
-    m1 = 1e-9
-)[[1]]
-sum(sols$value[sols$variable == "M" & sols$time == 2.01]) / 1e4
-D = exp(-2)
-D/s * (D^-s -1)
-solve_phi(0.9, 1, s)
-log_plot(sols)
 summary <- metric(summary, data)
-summary$theory <- fix_rate(summary$D, 1, s, N0, 1e-9)
+summary$theory <- theory(summary$D, r, s, summary$N0, m1)
 summary
-log_plot(data[[1]][[1]][data[[1]][[1]]$rep <= 10 & data[[1]][[1]]$time < 300, ])
-
-log_plot(simulate(deterministic = F, time = 100, mu = 1.024, dt = 1e-3, tau = 0.1, D = exp(-0.1), k=0, alpha = 0)[[1]])
-
+log_plot(data[[1]][[1]][data[[1]][[1]]$rep == 6 & data[[1]][[1]]$time < 100, ])
+sols <- data[[4]][[1]]
+sols2 <- sols[sols$variable == "W" & sols$time <= 0.24 & sols$rep == 1,]
+print(sols2, n=50)
 s <- 0.1
-N0 <- 1e9
-time <- 50
+N0 <- 1e8
+time <- 100
+m1 <- 1e-9
 
 # Wahl 1 No resource constraints
 r <- 1.024
-summary <- data.frame(D = 10 ^ - seq(0.1, 2, by = 0.2))
+summary <- expand.grid(D = 10 ^ - seq(0.1, 4, by = 4.1), N0 = 10 ^ seq(9, 9, 1))
 data <- list()
 for (i in seq_len(nrow(summary))) {
     D <- summary$D[i]
+    N0 <- summary$N0[i]
+    # m1 <- summary$m1[i]
     data[[i]] <- simulate(
         seed = NULL,
         rep = 1e3,
@@ -116,8 +116,9 @@ for (i in seq_len(nrow(summary))) {
         k = 0,
         alpha = 0,
         mu = c(W = r, M = r * (1 + s)),
-        init = c(W = round(N0 * D), M = 1),
-        m1 = 0
+        init_W = round(N0 * D),
+        m1 = 1e-9,
+        num_mutants = 10
     )
     print(i / nrow(summary))
 }
@@ -172,39 +173,49 @@ for (i in seq_len(nrow(summary))) {
 summary$wins <- 0
 for (i in seq_len(nrow(summary))) {
     # count the number of runs on which M was at least 1e-5 of W at the end
-    end <- data[[i]][[1]][data[[i]][[1]]$time == time, ]
+    end <- data[[i]][[1]][data[[i]][[1]]$time == max(data[[i]][[1]]$time), ]
     M <- end[end$variable == "M",]$value
-    W <- end[end$variable == "W",]$value
-    summary$wins[i] <- mean(ifelse(W==0, M, M / W) > 1e2 / N0)
-    rep <- data[[i]][[2]]$rep
-    ci <- binom.test(summary$wins[i] * rep, rep, conf.level = 0.95)$conf.int
-    summary$ci_lower[i] <- ci[1]
-    summary$ci_upper[i] <- ci[2]
+    summary$theta[i] <- mean(M) * data[[i]][[2]]$D
+    # W <- end[end$variable == "W",]$value
+    # summary$wins[i] <- mean(ifelse(W==0, M, M / W) > 1e2 / N0)
+    # summary$unsure[i] <- 1 - mean(M == 0) - summary$wins[i]
+    # rep <- data[[i]][[2]]$rep
+    # ci <- binom.test(summary$wins[i] * rep, rep, conf.level = 0.95)$conf.int
+    # summary$ci_lower[i] <- ci[1]
+    # summary$ci_upper[i] <- ci[2]
 }
 
-summary$phi <- solve_phi(summary$D, 1, s)
-summary$phi <- 1 + 2*s*log(summary$D)
-
-# png("wahl3.png", width = 12, height = 10, units = "in", res = 300)
-ggplot(summary, aes(x = D, y = time)) +
+summary$theory <- N0 * m1 * r * summary$D * (1 - phi(summary$D, s))
+summary$theory_old <- fix_rate(summary$D, r, s, N0, m1)
+summary$theory_full <- theory(summary$D, r, s, N0, m1)
+summary[c("D", "theory", "theory2")]
+png("images/test.png", width = 12, height = 10, units = "in", res = 300)
+ggplot(summary, aes(x = D, y = rate)) +
     geom_point(size = 3) +
     geom_errorbar(aes(ymin = ci_lower, ymax = ci_upper)) +
-    geom_line(aes(y = 1 / theory), color = "red") +
+    geom_line(aes(y = theory, color = "new")) +
+    geom_line(aes(y = theory_old, color = "old")) +
+    geom_line(aes(y = theory_full, color = "full")) +
+    scale_color_manual(values = c("new" = "red", "old" = "blue", "full" = "green")) +
     theme_light() +
     scale_x_log10() +
     scale_y_log10() +
     # scale_y_continuous(limits = c(0,1)) +
     labs(
-        title = "Optimal Dilution Ratio (resource unconstrained)",
-        x = "D",
-        y = "Hours per beneficial mutation that goes on to fix",
-        color = "Theory"
+        # title = "Optimal Dilution Ratio (resource unconstrained)",
+        x = "mutation rate",
+        y = "fixation rate",
+        color = "theoretical solution"
     ) +
     theme(
         plot.title = element_text(size = 26, face = "bold", hjust = 0.5),
         axis.title = element_text(size = 25, face = "bold"),
-        axis.text = element_text(size = 25)
+        axis.text = element_text(size = 25),
+        legend.title = element_text(size = 20),
+        legend.text = element_text(size = 20),
+        legend.position = "bottom"
     )
+dev.off()
 
 ggplot(summary, aes(x = D, y = wins)) +
     geom_point(size = 3) +
@@ -345,3 +356,8 @@ ggplot(summary, aes(x = D, y = tau)) +
 survival$rexp <- sort(rexp(100, 1 / summary$time[1]))
 plot(survival$rexp, sort(survival$t), log = "xy", xlim = c(1e-2, 5e1), ylim = c(1e-2, 5e1))
 abline(0, 1)
+
+# # Save the simulation results to a file
+save(data, file = "C:/Users/s4528540/Downloads/results/data_05_06.RData")
+# read the data file we just wrote back as data
+load("C:/Users/s4528540/Downloads/results/data4.RData")
