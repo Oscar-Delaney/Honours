4104550984dfb927bfb862e363f8316eaf4d1ac9
Oscar Delaney <o.delaney@uq.net.au>
2023-06-07 17:07:26 +1000

    wahl2 constant media resource conc

--- wahl/wahl_make_data.R
+++ wahl/wahl_make_data.R
@@ -9,7 +9,7 @@ hyper <- function(s, z) {
 
 # theoretical rate function
 theory <- function(D, r, s) {
-    return(-r / log(D) * (hyper(s,(1 - D) / (D * (D ^ s - 1))) -
+    return(-r / log(D) * (hyper(s, (1 - D) / (D * (D ^ s - 1))) -
         D * hyper(s, (1 - D) * (D ^ s) /  (D ^ s - 1))))
 }
 
@@ -54,11 +54,11 @@ metric <- function(summary, data) {
     return(summary)
 }
 
-summary <- metric(summary, data)
+summary <- metric(summary, data2)
 summary$theory <- theory(summary$D, r, s)
 summary
 
-log_plot(data[[5]][[1]][data[[1]][[1]]$rep <= 200 & data[[1]][[1]]$variable == "M50", ])
+log_plot(data2[[20]][[1]][data2[[1]][[1]]$rep == 1 & data2[[1]][[1]]$variable == "M100", ])
 
 data[[1]][[1]] %>%
     filter(rep == 1, variable == "W") %>%
@@ -68,9 +68,9 @@ data[[1]][[1]] %>%
 s <- 0.1
 time <- 50
 m1 <- 1e-9
+r <- 1.023
 
 # Wahl 1 No resource constraints
-r <- 1.023
 summary <- expand.grid(D = 10 ^ - seq(0.1, 4.0, by = 0.1))
 summary$N0 <- summary$D ^ - 0.5 * 10 ^ 9
 data <- list()
@@ -90,7 +90,6 @@ for (i in seq_len(nrow(summary))) {
         alpha = 0,
         mu = c(W = r, M = r * (1 + s)),
         init_W = round(N0 * D),
-        m1 = 1e-9,
         num_mutants = 1e2
     )
     print(i / nrow(summary))
@@ -98,29 +97,35 @@ for (i in seq_len(nrow(summary))) {
 
 # Wahl 2 Constant resource concentration in dilution media
 r <- 1.1
-# summary <- expand.grid(tau = seq(0.1, 3, 0.7), D = seq(0.1, 3, 0.7))
-summary <- data.frame(D = 10 ^ - seq(0.05, 4, by = 0.2))
+N0 <- 1e9
+k_ratio <- 1e-2
+summary <- expand.grid(D = 10 ^ - seq(0.1, 4, by = 0.6), tau = 24 * 2 ^ - seq(0, 3, by = 1))
+summary$m1 <- summary$D ^ - 0.5 * 1e-9
 data2 <- list()
 for (i in seq_len(nrow(summary))) {
     D <- summary$D[i]
+    tau <- summary$tau[i]
+    m1 <- summary$m1[i]
+    k <- k_ratio * N0
     data2[[i]] <- simulate(
         seed = NULL,
-        rep = 1e3,
+        rep = 1e1,
         time = time,
-        tau = - log(D),
+        dt = 1e-3,
+        tau = tau,
         D = D,
         N0 = N0,
-        k = 1e-2 * N0,
+        m1 = m1,
+        k = k,
         alpha = 1,
         mu = c(W = r, M = r * (1 + s)),
-        init = c(W = round(N0 * D), M = 0)
+        init_W = round(N0 * D),
+        num_mutants = 1e2
     )
     print(i/nrow(summary))
 }
 
 # Wahl 3 Constant total resource supply per time
-r <- 1.1
-# summary <- expand.grid(tau = seq(0.1, 2, 0.1), D = seq(0.1, 0.95, 0.05))
 summary <- data.frame(D = 10 ^ - seq(0.05, 4, by = 0.2))
 # summary$D <- exp(-summary$tau)
 data3 <- list()
@@ -141,31 +146,15 @@ for (i in seq_len(nrow(summary))) {
     print(i/nrow(summary))
 }
 
-# calculate summary statistics
-summary$wins <- 0
-for (i in seq_len(nrow(summary))) {
-    # count the number of runs on which M was at least 1e-5 of W at the end
-    end <- data[[i]][[1]][data[[i]][[1]]$time == max(data[[i]][[1]]$time), ]
-    M <- end[end$variable == "M",]$value
-    summary$theta[i] <- mean(M) * data[[i]][[2]]$D
-    # W <- end[end$variable == "W",]$value
-    # summary$wins[i] <- mean(ifelse(W==0, M, M / W) > 1e2 / N0)
-    # summary$unsure[i] <- 1 - mean(M == 0) - summary$wins[i]
-    # rep <- data[[i]][[2]]$rep
-    # ci <- binom.test(summary$wins[i] * rep, rep, conf.level = 0.95)$conf.int
-    # summary$ci_lower[i] <- ci[1]
-    # summary$ci_upper[i] <- ci[2]
-}
-
-summary$theory_approx <- r * summary$D * (1 - phi(summary$D, s))
-summary$theory_old <- solve_phi(summary$D, s) * theta(summary$D, s) * r / -log(summary$D)
-summary[c("D", "theory", "theory2")]
+summary <- metric(summary, data2)
+summary$k_ratio <- factor(round(summary$k_ratio, 2))
 # png("images/test.png", width = 12, height = 10, units = "in", res = 300)
-ggplot(summary, aes(x = D, y = rate)) +
+ggplot(summary, aes(x = D, y = rate, group = k_ratio, color = k_ratio)) +
     geom_point(size = 3) +
+    geom_line() +
     geom_errorbar(aes(ymin = ci_lower, ymax = ci_upper)) +
-    geom_line(aes(y = theory, color = "new")) +
-    scale_color_manual(values = c("new" = "red")) +
+    # geom_line(aes(y = theory, color = "new")) +
+    # scale_color_manual(values = c("new" = "red")) +
     theme_light() +
     scale_x_log10() +
     scale_y_log10() +
@@ -174,7 +163,7 @@ ggplot(summary, aes(x = D, y = rate)) +
         # title = "Optimal Dilution Ratio (resource unconstrained)",
         x = "mutation rate",
         y = "fixation rate (loci per hour per (mutations per generation))",
-        color = "theoretical solution"
+        color = "k_ratio"
     ) +
     theme(
         plot.title = element_text(size = 26, face = "bold", hjust = 0.5),
@@ -184,7 +173,6 @@ ggplot(summary, aes(x = D, y = rate)) +
         legend.text = element_text(size = 20),
         legend.position = "bottom"
     )
-# dev.off()
 
 ggplot(summary, aes(x = D, y = wins)) +
     geom_point(size = 3) +
@@ -238,8 +226,8 @@ summary$N_plus <- 0
 summary$N_minus <- 0
 for (i in seq_len(nrow(summary))) {
     # Find the time at which the final bottleneck occurred
-    sols <- data5[[i]][[1]]
-    sols <- sols[sols$variable == "N",]
+    sols <- data2[[i]][[1]]
+    sols <- sols[sols$variable == "N", ]
     last_bneck <- sols %>%
         filter(rep == 1) %>%
         filter(value > lag(value)) %>%
@@ -250,10 +238,9 @@ for (i in seq_len(nrow(summary))) {
     summary$N_minus[i] <- mean(sols[near(sols$time, last_bneck - dt), ]$value)
 }
 
-png("wahl_nutrients2_v2.png", width = 12, height = 10, units = "in", res = 300)
-ggplot(summary) +
-    geom_point(aes(x = D, y = N_minus, color = "Before"), size = 3) +
-    geom_point(aes(x = D, y = N_plus, color = "After"), size = 3) +
+ggplot(summary[summary$k_ratio == 0.1,]) +
+    geom_point(aes(x = D, y = N_minus, color = "Before", shape = k_ratio), size = 3) +
+    geom_point(aes(x = D, y = N_plus, color = "After", shape = k_ratio), size = 3) +
     theme_light() +
     scale_x_log10() +
     # scale_y_log10() +
@@ -279,32 +266,6 @@ ggplot(summary) +
     )
 dev.off()
 
-filtered <- data5[[4]][[1]] %>%
-    filter(variable %in% c("N","W")) %>%
-    filter(time > 0)
-
-ggplot() +
-    geom_point(data = filtered, aes(x = time, y = value, color = variable)) +
-    scale_y_continuous(trans = scales::pseudo_log_trans(base = 10),
-        breaks = 10^seq(0, 20),
-        labels = scales::trans_format("log10", scales::math_format(10^.x))) +
-    labs(
-        title = "Bacterial growth over time",
-        x = "Time (hours)",
-        y = "Population Size",
-        color = "Strain",
-        fill = "Strain"
-    ) +
-    theme_light() +
-    theme(
-        legend.position = "bottom",
-        plot.title = element_text(size = 35, face = "bold", hjust = 0.5),
-        axis.title = element_text(size = 25, face = "bold"),
-        axis.text = element_text(size = 25),
-        legend.title = element_text(size = 20),
-        legend.text = element_text(size = 20)
-    )
-
 ggplot(summary, aes(x = D, y = tau)) +
     geom_tile(aes(fill = wins)) +
     scale_fill_gradient(low = "white", high = "blue", limits = c(0, 1)) +
@@ -320,11 +281,6 @@ ggplot(summary, aes(x = D, y = tau)) +
         legend.title = element_text(size = 20),
         legend.text = element_text(size = 20)
     )
-
-# unused
-survival$rexp <- sort(rexp(100, 1 / summary$time[1]))
-plot(survival$rexp, sort(survival$t), log = "xy", xlim = c(1e-2, 5e1), ylim = c(1e-2, 5e1))
-abline(0, 1)
 
 # # Save the simulation results to a file
 save(data, file = "C:/Users/s4528540/Downloads/results/data_05_06.RData")
