c4debdf568e244ffa949de9648a5498379d6c812
Oscar Delaney <o.delaney@uq.net.au>
2023-08-08 16:21:57 +1000

    allowed summarize = TRUE in run_sims pared back number of necessary functions

--- wahl/data_analysis.R
+++ wahl/data_analysis.R
@@ -1,18 +1,16 @@
 # Generate and analyse data
-run_sims <- function(summary, rep = 1, time = 50, w = 0.1, r = 1, mu = 1e-9,
-    res = TRUE, flow = 1, num_mutants = 1e2, loci = NULL) {
-    func <- ifelse(is.null(loci), metric, metric_ci)
-    data <- list()
+run_sims <- function(summary, rep = 1, time = 50, w = 0.1, r = 1, mu = 1e-9, dt = 1e-2,
+    res = TRUE, flow = 1, num_mutants = 1e2, loci = NULL, summarize = TRUE) {
     for (i in seq_len(nrow(summary))) {
         D <- summary$D[i]
         tau <- summary$tau[i]
         k_ratio <- res *
             ifelse("k_ratio" %in% names(summary), summary$k_ratio[i], 1)
         data <- simulate(
-            # seed = i,
+            seed = i,
             rep = rep,
             time = time,
-            dt = 1e-2,
+            dt = dt,
             tau = ifelse(tau == 0, 1e4, tau),
             max_step = Inf,
             D = D,
@@ -20,16 +18,27 @@ run_sims <- function(summary, rep = 1, time = 50, w = 0.1, r = 1, mu = 1e-9,
             media = 1e9,
             k = 1e9 * k_ratio,
             alpha = 1 * res,
-            r = r * (1 + k_ratio), # * ifelse(D == 1, 1, 1.023),
-            # Adaptivetau causes observed growth rate to be lower than expected
+            r = r * (1 + k_ratio),
             w = w,
             mu = mu,
             N = 1e9,
             num_mutants = num_mutants,
-            loci = loci
+            loci = loci,
+            summarize = summarize
         )
-        summary[i, c("rate", "ci_lower", "ci_upper")] <- func(data)
-        # summary[i, "total_pop"] <- log10(mean(total_pop(data[[1]], strains = "W")))
+        if (summarize) {
+            fixed <- data[[1]] %>%
+                group_by(rep) %>%
+                summarise(n = sum(final_value > 1e1), n_hat = sum(p_fix))
+            # estimate the fixation rate and store this
+            fixation_rate <- fixed$n_hat / data[[2]]$endpoint
+            se <- sd(fixation_rate) / sqrt(length(fixation_rate))
+            ci <- mean(fixation_rate) + se * qnorm(c(0.5, 0.025, 0.975))
+        } else {
+            func <- ifelse(is.null(loci), metric, metric_ci)
+            ci <- func(data)
+        }
+        summary[i, c("rate", "ci_lower", "ci_upper")] <- ci
         print(i / nrow(summary))
     }
     return(summary)
@@ -41,74 +50,19 @@ phi <- function(D, s) {
     return(ifelse(D == 1, 1 / (1 + s), (1 - D) / (D ^ -s - D)))
 }
 
-# Hypergeometric function
-hyper <- function(w, z) {
-    return(Re(hypergeo(1, 1 / (1 + w), 1 + 1 / (1 + w), z)))
-}
-
-# theoretical rate function
-theory <- function(D, r, w) {
-    return(-r / log(D) * (hyper(w, (1 - D) / (D * (D ^ w - 1))) -
-        D * hyper(w, (1 - D) * (D ^ w) /  (D ^ w - 1))))
-}
-
-# Approximate rate function
-approx1_theory <- function(D, r, w) {
-    return(r * w / (1 + w) * ifelse(D == 1, 1, log(D^-1) / (D^-1 -1)))
-}
-
-# Even more approximate rate function
-approx2_theory <- function(D, r, w) {
-    return(r * w / (1 + w) * sqrt(D))
-}
-
 # theoretical resource constrained adaptation rate
-theory_res <- function(D, tau, r, w = 0.1, media = 1e9, k = 1e9, mu = 1e-9, flow = 1) {
-    k <- rep(k, length(D))[1:length(D)]
-    N <- ifelse(k == 0, media, find_W(r, D, media, k, tau, flow))
+theory <- function(D, tau, r, w = 0.1, media = 1e9, k = 1e9, mu = 1e-9, flow = 1, alpha = 1) {
+    k <- rep(k, length(D))[seq_along(length(D))]
+    N <- ifelse(k == 0 | alpha == 0, media, find_W(r, D, media, k, tau, flow, alpha))
     x <- ifelse(D == 1 | tau == 0, flow, log(D) ^ 2 / (1 / D - 1) / tau)
-    return(w / (1 + w) * mu * N * x) #  / (1 + w)
+    return(w / (1 + w) * mu * N * x)
 }
 
 # rate at a given time within [0, tau)
 rate_at_t <- function(D, r, w, t) {
-  (D * (-1 + D^w) * exp(r * t) * r) /
-    (-1 + D^(1 + w) * exp(r * (1 + w) * t) - D^w * (-1 + exp(r * (1 + w) * t)))
-}
-
-# Count the number of mutants likely en route to fixation
-fixed <- function(data) {
-    # find the time just after the last bottleneck
-    endpoint <- min(data[[2]]$time, data[[1]] %>%
-        filter(variable == "W", rep == 1) %>%
-        filter(value - lag(value) < 0) %>%
-        tail(1) %>%
-        pull(time))
-    # find the likelihood of a new mutation at t=0 going extinct
-    current_phi <- phi(data[[2]]$D, data[[2]]$w)
-    # count the number of each mutant at the endpoint
-    final_counts <- data[[1]] %>%
-        group_by(rep, variable) %>%
-        filter(time == endpoint, !(variable %in% c("W", "R"))) %>%
-        summarise(final_value = value, .groups = "keep") %>%
-        mutate(p_fix = 1 - current_phi ^ final_value)
-    return(list(final_counts, endpoint))
-}
-
-# evaluate a set of simulation results
-metric <- function(data) {
-    input <- fixed(data)
-    final_counts <- input[[1]]
-    endpoint <- input[[2]]
-    # estimate the number of these mutations that will go on to fix
-    fixed <- final_counts %>%
-        group_by(rep) %>%
-        summarise(n = sum(final_value > 1e1), n_hat = sum(p_fix))
-    # estimate the fixation rate and store this
-    fixation_rate <- fixed$n_hat / endpoint
-    se <- sd(fixation_rate) / sqrt(length(fixation_rate))
-    ci <- mean(fixation_rate) + se * qnorm(c(0.5, 0.025, 0.975))
-    return(ci)
+    u <- D * exp(r * t)
+    theta <- (1 / D - 1) / (1 - D ^ w)
+    return(r^2 / -log(D) * (u / (1 + theta * u ^ (1 + w))))
 }
 
 metric_ci <- function(data) {
