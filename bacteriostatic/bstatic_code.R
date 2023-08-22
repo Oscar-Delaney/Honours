2243f7d7cf0b4d098e572d54da41b1b3b9b5afb0
Oscar Delaney <o.delaney@uq.net.au>
2023-08-22 16:49:33 +1000

    introduce intermediate resource treatment minor aesthetic tweaks otehrwise

--- bacteriostatic/bstatic_code.R
+++ bacteriostatic/bstatic_code.R
@@ -36,16 +36,19 @@ target_hit <- function(sol, target = 1, strains = c("S", "RA", "RB", "RAB")) {
 }
 
 run_sims <- function(summary, zeta1 = c(S = 1, RA = 28, RB = 1, RAB = 28),
-zeta2 = c(S = 1, RA = 1, RB = 28, RAB = 28), delta = 0.3, rep = 1, dose_gap = 10,
-influx = 3 * c(A = 1, B = 1), m_A = 1e-9, m_B = 1e-9, d_ = 0, data = FALSE) {
+zeta2 = c(S = 1, RA = 1, RB = 28, RAB = 28), delta = 0.25, rep = 1, dose_gap = 10,
+influx = 3 * c(A = 1, B = 1), m_A = 1e-9, m_B = 1e-9, d_ = 0, init_A = 0,
+init_B = 0, N0 = 1e8, data = FALSE) {
     summary$bstatic1 <- 1 - summary$bcidal1
     summary$bstatic2 <- 1 - summary$bcidal2
     for (i in seq_len(nrow(summary))) {
         cycl <- ifelse(summary$therapy[i] == "Cycling", TRUE, FALSE)
+        res <- switch(as.character(summary$resources[i]),
+            "Abundant" = 3, "Intermediate" = 1.5, "Limiting" = 0)
         d <- d_ + ifelse(cycl, 0.1, 0.35)
         sol <- simulate(
-            init = c(S = ifelse(cycl, 5e8, 1e10), RA = 0, RB = 0, RAB = 0),
-            N0 = ifelse(summary$resources[i] == "Abundant", 1e11, 1e8),
+            init = c(S = ifelse(cycl, 5e8, 1e10), RA = init_A, RB = init_B, RAB = 0),
+            N0 = N0 * 10 ^ res,
             k = 1e8,
             alpha = 1,
             supply = 1e8,
@@ -56,7 +59,7 @@ influx = 3 * c(A = 1, B = 1), m_A = 1e-9, m_B = 1e-9, d_ = 0, data = FALSE) {
             bstatic2 = summary$bstatic2[i],
             zeta1 = zeta1,
             zeta2 = zeta2,
-            delta = ifelse(summary$resources[i] == "Abundant", 0.4, 0.3),
+            delta = delta + res * 0.05,
             time = 60,
             tau = 1e4,
             rep = rep,
@@ -69,9 +72,6 @@ influx = 3 * c(A = 1, B = 1), m_A = 1e-9, m_B = 1e-9, d_ = 0, data = FALSE) {
         wins <- 1 - target_hit(sol, target = 1e2, strains = c("RA", "RB"))
         summary[i, c("wins", "ymin", "ymax")] <- c(mean(wins),
             binom.test(sum(wins), length(wins))$conf.int)
-        # total <- total_pop(sol)
-        # summary[i, c("pop", "pop_wmin", "pop_ymax")] <- c(mean(total),
-        #     t.test(total)$conf.int)
         print(i / nrow(summary))
     }
     if(data){
@@ -82,8 +82,8 @@ influx = 3 * c(A = 1, B = 1), m_A = 1e-9, m_B = 1e-9, d_ = 0, data = FALSE) {
 }
 
 main_plot <- function(summary) {
-    labels <- expand.grid(bcidal1 = 0, bcidal2 = 1, therapy = c("Cycling", "Combination"), resources = c("Abundant", "Limiting"))
-    labels$label <- c("A", "B", "C", "D")
+    labels <- expand.grid(bcidal1 = 0, bcidal2 = 1, therapy = unique(summary$therapy), resources = unique(summary$resources))
+    labels$label <- LETTERS[1:nrow(labels)]
     p <- ggplot(summary, aes(x = bcidal1, y = bcidal2)) +
         geom_tile(aes(fill = wins)) +
         scale_fill_gradient(low = "white", high = "blue", limits = c(0, 1)) +
@@ -97,9 +97,9 @@ main_plot <- function(summary) {
             legend.text = element_text(size = 20),
             legend.position = "bottom",
             legend.key.size = unit(2, "cm"),
-            strip.text = element_text(size = 25, face = "bold")  # Increase facet label sizes
+            strip.text = element_text(size = 25, face = "bold")
         )
-    if(nrow(unique(summary[, c("therapy", "resources")])) > 1){
+    if (nrow(unique(summary[, c("therapy", "resources")])) > 1) {
         p <- p + geom_text(data = labels, aes(label = label), vjust = 0.7, hjust = 0.7, size = 15, fontface = "bold")
     }
     return(p)
@@ -129,8 +129,6 @@ mono_plot <- function(summary, series, lower, upper, ylab, text){
     return(p)
 }
 
-data_dir <- "C:/Users/s4528540/Downloads/results/static"
-
 ### Figure 1
 summary <- expand.grid(bcidal1 = seq(0, 1, 0.05), bcidal2 = 0,
     therapy = "Cycling", resources = "Abundant")
@@ -141,30 +139,29 @@ sol <- run_sims(summary[nrow(summary), ], delta = 0.4, rep = 1e1,
 dynamics <- log_plot(sol, use = c("S", "RA", "RB", "RAB", "N")) +
     annotate("text", x = 0, y = Inf, label = "A", hjust = 0.5, vjust = 1,
         size = 15, fontface = "bold")
-# mono1 <- mono_plot(mono_high_res, "pop", "pop_wmin", "pop_ymax", "Total S population", "B")
-mono2 <- mono_plot(mono_high_res, "wins", "ymin", "ymax", "P(extinct)", "B")
+mono <- mono_plot(mono_high_res, "wins", "ymin", "ymax", "P(extinct)", "B")
 
 # print as a pdf
 pdf("bacteriostatic/fig1.pdf", width = 20, height = 10)
-grid.arrange(dynamics, mono2, ncol = 2)
+grid.arrange(dynamics, mono, ncol = 2)
 dev.off()
 
-save(mono_high_res, file = paste0(data_dir, "/fig1.rdata"))
+save(mono_high_res, file = "bacteriostatic/fig1.rdata")
 
 ### Figure 2
 summary <- expand.grid(bcidal1 = seq(0, 1, 0.05), bcidal2 = seq(0, 1, 0.05),
-    therapy = c("Cycling", "Combination"), resources = c("Abundant", "Limiting"))
+    therapy = c("Combination", "Cycling"), resources = c("Abundant", "Intermediate", "Limiting"))
 summary <- run_sims(summary, rep = 1e3)
 
 pdf("bacteriostatic/fig2.pdf", width = 20, height = 20)
 main_plot(summary)
 dev.off()
 
-save(summary, file = paste0(data_dir, "/fig2.rdata"))
+save(summary, file = "bacteriostatic/fig2.rdata")
 
 ### Figure S1
 summary <- expand.grid(bcidal1 = seq(0, 1, 0.05), bcidal2 = seq(0, 1, 0.05),
-    therapy = c("Cycling"), resources = c("Abundant")) 
+    therapy = c("Cycling"), resources = c("Abundant"))
 cs <- run_sims(summary, rep = 1e3, zeta1 = c(S = 1, RA = 28, RB = 0.5, RAB = 28),
     zeta2 = c(S = 1, RA = 0.5, RB = 28, RAB = 28), delta = 0.25)
 
@@ -172,13 +169,65 @@ pdf("bacteriostatic/figS1.pdf", width = 10, height = 10)
 main_plot(cs)
 dev.off()
 
-save(cs, file = paste0(data_dir, "/figS1.rdata"))
+save(cs, file = "bacteriostatic/figS1.rdata")
 
 ### Figure S2
-quick_degrade <- run_sims(summary, rep = 1e3, influx = 10 * c(A = 1, B = 1), d_ = 0.4, delta = 0.6)
-
+quick_degrade <- run_sims(summary, rep = 1e3, influx = 30 * c(A = 1, B = 1), d_ = 0.4, delta = 0.4)
 pdf("bacteriostatic/figS2.pdf", width = 10, height = 10)
 main_plot(quick_degrade)
 dev.off()
 
-save(quick_degrade, file = paste0(data_dir, "/figS2.rdata"))
+save(quick_degrade, "bacteriostatic/figS2.rdata")
+
+### Figure S3
+pre_existing <- run_sims(summary, rep = 1e3, m_A = 0, m_B = 0, init_B = 5, delta = 0.1)
+
+pdf("bacteriostatic/figS3.pdf", width = 10, height = 10)
+main_plot(pre_existing)
+dev.off()
+
+save(pre_existing, file = "bacteriostatic/figS3.rdata")
+
+# Function to create a single plot given a subset of the data and title labels
+create_plot <- function(data_subset, label) {
+  p <- ggplot(data_subset, aes(x = bcidal1, y = bcidal2)) +
+    geom_tile(aes(fill = wins)) +
+    scale_fill_gradient(low = "white", high = "blue") +
+    labs(x = expression(theta["A"]), y = expression(theta["B"]), fill = "P(extinct)") +
+    theme_minimal() +
+    theme(
+      axis.title = element_text(size = 35, face = "bold"),
+      axis.text = element_text(size = 25),
+      legend.title = element_text(size = 25),
+      legend.text = element_text(size = 20),
+      legend.position = "bottom",
+      legend.key.size = unit(2, "cm"),
+      strip.text = element_text(size = 25, face = "bold")
+    )
+  if (nrow(unique(data_subset[, c("therapy", "resources")])) == 1) {
+    print(label)
+    p <- p + geom_text(aes(x = 0, y = 1, label = label), vjust = 0.5, hjust = 0.5, size = 15, fontface = "bold")
+  }
+  return(p)
+}
+
+combined_plot <- function(summary) {
+  plots <- list()
+
+  therapies <- c("Combination", "Cycling")
+  resources <- c("Abundant", "Limiting")
+  labels <- c("A", "B", "C", "D")
+  idx <- 1
+
+  for (r in resources) {
+    for (t in therapies) {
+      subset_data <- subset(summary, therapy == t & resources == r)
+      plots[[paste(t, r)]] <- create_plot(subset_data, labels[idx])
+      idx <- idx + 1
+    }
+  }
+
+  grid.arrange(plots[[1]], plots[[2]], plots[[3]], plots[[4]], ncol = 2)
+}
+
+combined_plot(summary)
