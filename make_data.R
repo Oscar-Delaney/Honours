2a61b6f96fd7a4c6a6de602a58ccabe5632510ed
Oscar Delaney <o.delaney@uq.net.au>
2023-08-30 16:28:19 +1000

    update notation 1, 2 --> A, B return to different mutation rates analysis

--- make_data.R
+++ make_data.R
@@ -1,7 +1,7 @@
 source("stochastic.R")
 
 # Total population
-total_pop <- function(sol, dt = 1, strains = c("S", "R1", "R2", "R12")) {
+total_pop <- function(sol, dt = 1, strains = c("N_S", "N_A", "N_B", "N_AB")) {
   sol %>%
     filter(variable %in% strains) %>%
     group_by(rep) %>%
@@ -10,7 +10,7 @@ total_pop <- function(sol, dt = 1, strains = c("S", "R1", "R2", "R12")) {
 }
 
 # Final population
-final_pop <- function(sol, strains = c("S", "R1", "R2", "R12")) {
+final_pop <- function(sol, strains = c("N_S", "N_A", "N_B", "N_AB")) {
   sol %>%
     filter(time == max(time) & variable %in% strains) %>%
     group_by(rep) %>%
@@ -19,7 +19,7 @@ final_pop <- function(sol, strains = c("S", "R1", "R2", "R12")) {
 }
 
 # Time to reach target
-target_time <- function(sol, target = 1, strains = c("S", "R1", "R2", "R12")) {
+target_time <- function(sol, target = 1, strains = c("N_S", "N_A", "N_B", "N_AB")) {
   sol %>%
     filter(variable %in% strains) %>%
     group_by(rep, time) %>%
@@ -30,60 +30,58 @@ target_time <- function(sol, target = 1, strains = c("S", "R1", "R2", "R12")) {
 }
 
 # Whether target was hit
-target_hit <- function(sol, target = 1, strains = c("S", "R1", "R2", "R12")) {
+target_hit <- function(sol, target = 1, strains = c("N_S", "N_A", "N_B", "N_AB")) {
   !is.na(target_time(sol, target, strains))
 }
 
 # Create a grid of parameter combinations
 # summary <- expand.grid(dose_rep = seq(1, 12, 1), kappa = seq(0.5, 3, 0.5))
-auc <- 20
-gap <- 10
-summary <- expand.grid(d = seq(0, 0.4, 0.03), kappa = seq(0.5, 3, 0.5))
-summary$c0 <- auc * with(summary, ifelse(d == 0, 1 / gap, d / (1 - exp(-d * gap))))
+summary <- expand.grid(cA = seq(0.5, 1, 0.05), m_A = c(2 ^ - seq(1, 6, 1), 0))
+summary$m_A_discrete <- as.factor(round(summary$m_A, 3))
 data <- list()
 for (i in seq_len(nrow(summary))) {
     data[[i]] <- simulate(
-        init = c(S = 3e8, RA = 0, RB = 0, RAB = 0),
-        N0 = 1e9,
+        init = c(N_S = 1e10, N_A = 0, N_B = 0, N_AB = 0),
+        R0 = 1e10,
         k = 0,
         alpha = 0,
         supply = 1e8,
         mu = 1,
-        bcidal1 = 2,
-        bcidal2 = 2,
+        bcidal_A = 2,
+        bcidal_B = 2,
+        delta = 0,
         time = 40,
         tau = 1e4,
         max_step = 1e-1,
-        rep = 1e3,
+        rep = 2e2,
         HGT = 0,
         dose_rep = 1,
-        dose_gap = gap,
-        influx = summary$c0[i] * c(A = 1, B = 1),
-        cycl = TRUE,
-        keep_old_drugs = FALSE,
-        d1 = summary$d[i], d2 = summary$d[i],
-        deterministic = FALSE,
-        kappa1 = summary$kappa[i], kappa2 = summary$kappa[i]
+        dose_gap = 1e4,
+        influx = 1.5 * c(C_A = summary$cA[i], C_B = 1 - summary$cA[i]),
+        cycl = FALSE,
+        m_A = 1e-9 * summary$m_A[i], m_B = 1e-9 * (1 - summary$m_A[i]),
+        d_A = 0, d_B = 0,
+        deterministic = FALSE
     )
-    wins <- target_hit(data[[i]][[1]], target = 1e2, strains = c("RA", "RB"))
+    wins <- !target_hit(data[[i]][[1]], target = 1e2, strains = c("N_A", "N_B"))
     summary$wins[i] <- mean(wins)
     ci <- binom.test(sum(wins), length(wins), conf.level = 0.95)$conf.int
     summary$ymin[i] <- ci[1]
     summary$ymax[i] <- ci[2]
     print(i / nrow(summary))
 }
-log_plot(data[[4]][[1]][data[[1]][[1]]$rep <= 10, ])
+log_plot(data[[11]][[1]][data[[1]][[1]]$rep <= 10, ])
 
 # plot with one independent variable and one dependent variable
-ggplot(summary, aes(x = d, y = wins, color = as.factor(round(kappa, 3)))) +
+ggplot(summary, aes(x = cA, y = wins, color = m_A_discrete)) +
     geom_point(size = 3) +
     geom_errorbar(aes(ymin = ymin, ymax = ymax)) +
     theme_light() +
     scale_y_continuous(limits = c(0, 1)) +
     labs(
-        x = "Drug elimination rate (d)",
-        y = "Probability that resistance emerges",
-        color = "Kappa"
+        x = "proportion of dose that is A",
+        y = "P(extinct))",
+        color = "m_A"
     ) +
     theme(
         plot.title = element_text(size = 32, face = "bold", hjust = 0.5),
@@ -94,15 +92,37 @@ ggplot(summary, aes(x = d, y = wins, color = as.factor(round(kappa, 3)))) +
         legend.position = "bottom"
     )
 
+# plot with two independent variables and one dependent variable
+ggplot(summary, aes(x = log2(m_A), y = cA)) +
+    geom_tile(aes(fill = wins)) +
+    labs(fill = "P(extinct)") +
+    theme_minimal() +
+    labs(
+        x = "Log2 proportion of de novo mutations that are A",
+        y = "Proportion of dose that is drug A",
+        size = "Probability that MDR emerges"
+    ) +
+    scale_fill_gradient(low = "white", high = "blue", limits = c(0, 1)) +
+    theme(
+        axis.title = element_text(size = 25, face = "bold"),
+        axis.text = element_text(size = 25),
+        legend.title = element_text(size = 20),
+        legend.text = element_text(size = 20),
+        legend.position = "bottom",
+        legend.key.width = unit(2, "cm"),
+        legend.spacing.x = unit(1, "cm"),
+        strip.text = element_text(size = 25, face = "bold")
+    )
+    
 summary
 sol <- data[[1]][[1]][data[[1]][[1]]$rep == 43, ]
-sol2 <- sol[sol$variable =="S" & sol$time <= 15,]
+sol2 <- sol[sol$variable =="N_S" & sol$time <= 15,]
 plot(sol2$time, sol2$value, type = "l")
-single_run(config, 1)[, c("time", "S", "N", "A1", "A2")]
+single_run(config, 1)[, c("time", "N_S", "N", "A1", "A2")]
 sol
 # Calculate the summary statistics
 for (i in seq_len(nrow(summary))) {
-    summary$wins[i] <- mean(final_pop(data[[i]][[1]], c("R1", "R2")) > 1e2)
+    summary$wins[i] <- mean(final_pop(data[[i]][[1]], c("N_A", "N_B")) > 1e2)
     rep <- data[[i]][[2]]$rep
     ci <- binom.test(summary$wins[i] * rep, rep, conf.level = 0.95)$conf.int
     summary$ymin[i] <- ci[1]
@@ -110,7 +130,7 @@ for (i in seq_len(nrow(summary))) {
 }
 
 for (i in seq_len(nrow(summary))) {
-    pops <- total_pop(data[[i]][[1]], strains = "S")
+    pops <- total_pop(data[[i]][[1]], strains = "N_S")
     summary$pop[i] <- mean(pops)
     ci <- t.test(pops, conf.level = 0.95)$conf.int
     summary$ymin[i] <- ci[1]
@@ -118,7 +138,7 @@ for (i in seq_len(nrow(summary))) {
 }
 
 for (i in seq_len(nrow(summary))) {
-    wins <- target_hit(data[[i]][[1]], target = 1e2, strains = "R1")
+    wins <- target_hit(data[[i]][[1]], target = 1e2, strains = "N_A")
     summary$wins[i] <- mean(wins)
     ci <- binom.test(sum(wins), length(wins), conf.level = 0.95)$conf.int
     summary$ymin[i] <- ci[1]
@@ -126,26 +146,7 @@ for (i in seq_len(nrow(summary))) {
 }
 
 
-# plot with two independent variables and one dependent variable
-ggplot(summary, aes(x = D, y = HGT, size = wins)) +
-    geom_point() +
-    scale_x_log10() +
-    scale_y_log10() +
-    theme_minimal() +
-    labs(
-        title = "Impact of D and HGT on MDR emergence",
-        x = "Dilution fraction D",
-        y = "Recombination rate",
-        size = "Probability that MDR emerges"
-    ) +
-    theme(
-        legend.position = "bottom",
-        plot.title = element_text(size = 35, face = "bold", hjust = 0.5),
-        axis.title = element_text(size = 25, face = "bold"),
-        axis.text = element_text(size = 25),
-        legend.title = element_text(size = 20),
-        legend.text = element_text(size = 20)
-    )
+
 
 # Next I will make a plot similar to that above, but with a colour scale
 ggplot(summary, aes(x = D, y = HGT, color = wins)) +
