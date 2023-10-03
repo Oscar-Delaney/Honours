796c4812bfb5d4a8b026a37ff4b9e2f58d72460c
Oscar Delaney <o.delaney@uq.net.au>
2023-10-03 10:16:07 +1000

    Experiments for mutation rate varying project Sorry Git, many changes, this is all experimental so matters less

--- make_data.R
+++ make_data.R
@@ -34,45 +34,144 @@ target_hit <- function(sol, target = 1, strains = c("N_S", "N_A", "N_B", "N_AB")
   !is.na(target_time(sol, target, strains))
 }
 
+theory_d <- function(C, zeta = 1) {
+    1 / (1 + zeta / C)
+}
+
+theory_N <- function(delta, d_A, d_B, mu, init_S, bcidal, bstatic) {
+    mu <- mu * (1 - bstatic * d_A) * (1 - bstatic * d_B)
+    N <- init_S * mu / (delta + bcidal * (d_A + d_B) - mu)
+    ifelse(N < 0, Inf, N)
+}
+
+theory_phi <- function(m_A, c_A, c_B, bcidal, bstatic, delta, zeta, mu) {
+    d_A <- theory_d(c_A, 1)
+    d_A_R <- theory_d(c_A, zeta)
+    d_B <- theory_d(c_B, 1)
+    d_B_R <- theory_d(c_B, zeta)
+    pmin(1, m_A * (delta + bcidal * (d_B + d_A_R)) / (mu * (1 - bstatic * d_A_R) * (1 - bstatic * d_B)) +
+    (1 - m_A) * (delta + bcidal * (d_A + d_B_R)) / (mu * (1 - bstatic * d_A) * (1 - bstatic * d_B_R)))
+}
+
+theory_extinct <- function(phi, N, m) {
+    (1 - m * (1 - phi)) ^ N
+}
+
+summary$phi <- theory_phi(summary$m_A, C_tot * summary$cA, C_tot * (1 - summary$cA), bcidal, bstatic, delta, zeta, mu)
+summary$N <- theory_N(delta, theory_d(C_tot * summary$cA), theory_d(C_tot * (1 - summary$cA)), mu, init_S, bcidal, bstatic)
+summary$extinct <- theory_extinct(summary$phi, summary$N, m)
+
+
+# plot with two independent variables and one dependent variable
+summary_plot <- function(summary, var) {
+  # Find the y-values that maximize 'var' for each x-value
+  summary$approx <- 1 / (1 + (1 / summary$m_A - 1) ^ -0.5)
+  max_df <- summary %>%
+    group_by(log2(m_A)) %>%
+    arrange(desc(get(var))) %>%
+    slice_head(n = 1) %>%
+    ungroup()
+
+    ggplot(summary, aes(x = log2(m_A), y = cA)) +
+    geom_tile(aes(fill = (get(var)))) +
+    geom_line(aes(x = log2(m_A), y = approx), color = "green", linewidth = 2) +
+    geom_point(data = max_df, aes(y = cA, x = log2(m_A)), color = "yellow", size = 5) +
+    labs(fill = "P(extinct)") +
+    theme_minimal() +
+    labs(
+        x = "Log2 proportion of de novo mutations that are A",
+        y = "Proportion of dose that is drug A",
+        size = "Probability that MDR emerges"
+    ) +
+    scale_fill_gradient(low = "white", high = "blue", limits = c(0, 1)) +
+    theme(
+        axis.title = element_text(size = 25, face = "bold"),
+        axis.text = element_text(size = 25),
+        legend.title = element_text(size = 20),
+        legend.text = element_text(size = 20),
+        legend.position = "bottom",
+        legend.key.width = unit(2, "cm"),
+        legend.spacing.x = unit(1, "cm"),
+        strip.text = element_text(size = 25, face = "bold")
+    )
+}
+summary_plot(summary, "N")
+
 # Create a grid of parameter combinations
 # summary <- expand.grid(dose_rep = seq(1, 12, 1), kappa = seq(0.5, 3, 0.5))
-summary <- expand.grid(cA = seq(0.5, 1, 0.05), m_A = c(2 ^ - seq(1, 6, 1), 0))
-summary$m_A_discrete <- as.factor(round(summary$m_A, 3))
-data <- list()
+init_S <- 1e9
+m <- 1e-9
+zeta <- 1e9
+
+
+summary <- expand.grid(cA = seq(0.5, 1, 0.01), m_A = c(2 ^ -seq(1, 20, 0.2)))
+
+c <- 3
+th <- 1.001 + c^-5
+# data <- list()
 for (i in seq_len(nrow(summary))) {
     data[[i]] <- simulate(
-        init = c(N_S = 1e10, N_A = 0, N_B = 0, N_AB = 0),
+        init = c(N_S = init_S, N_A = 0, N_B = 0, N_AB = 0),
         R0 = 1e10,
         k = 0,
         alpha = 0,
         supply = 1e8,
         mu = 1,
-        bcidal_A = 2,
-        bcidal_B = 2,
-        delta = 0,
-        time = 40,
+        bcidal_A = th,
+        bcidal_B = th,
+        bstatic_A = 0,
+        bstatic_B = 0,
+        zeta_A = c(N_S = 1, N_A = zeta, N_B = 1, N_AB = 1e9),
+        zeta_B = c(N_S = 1, N_A = 1, N_B = zeta, N_AB = 1e9),
+        delta = 0, # 1 - th / (1 + 1 / c),
+        time = 50,
         tau = 1e4,
         max_step = 1e-1,
-        rep = 2e2,
+        kappa_A = 5, kappa_B = 5,
+        rep = 1e1,
         HGT = 0,
         dose_rep = 1,
         dose_gap = 1e4,
-        influx = 1.5 * c(C_A = summary$cA[i], C_B = 1 - summary$cA[i]),
+        influx = c * c(C_A = summary$cA[i], C_B = 1 - summary$cA[i]),
         cycl = FALSE,
-        m_A = 1e-9 * summary$m_A[i], m_B = 1e-9 * (1 - summary$m_A[i]),
+        m_A = m * summary$m_A[i], m_B = m * (1 - summary$m_A[i]),
         d_A = 0, d_B = 0,
-        deterministic = FALSE
+        deterministic = FALSE,
+        config_only = TRUE
     )
-    wins <- !target_hit(data[[i]][[1]], target = 1e2, strains = c("N_A", "N_B"))
+    v <- with(data[[i]], as.list(rates(c(N_S = 1, N_A = 1, N_B = 1, N_AB = 1, R = R0, influx * pattern), data[[i]], 0)))
+    summary$phi[i] <- with(v, summary$m_A[i] * pmin(1, N_A_death / N_A_growth) +
+        (1 - summary$m_A[i]) * pmin(1, N_B_death / N_B_growth))
+    summary$N[i] <- with(v, init_S / (pmax(1e-30, S_death / S_growth - 1)))
+    summary$extinct[i] <- (1 - m * (1 - summary$phi[i])) ^ summary$N[i]
+    summary$extinct2[i] <- exp(-m * (1 - summary$phi[i]) * summary$N[i])
+    # wins <- !target_hit(data[[i]][[1]], target = 1e3, strains = c("N_A", "N_B"))
+    # summary$wins[i] <- mean(wins)
+    # ci <- binom.test(sum(wins), length(wins), conf.level = 0.95)$conf.int
+    # summary$ymin[i] <- ci[1]
+    # summary$ymax[i] <- ci[2]
+    # print(i / nrow(summary))
+}
+summary_plot(summary, "extinct2")
+summary[summary$m_A == 2 ^ - 1.01,]
+hist(log10(summary$extinct2 / summary$extinct))
+tail(summary)
+summary_plot(summary, "wins")
+for (i in seq_len(nrow(summary))) {
+    wins <- !target_hit(data[[i]][[1]], target = 1e3, strains = c("N_A", "N_B"))
     summary$wins[i] <- mean(wins)
     ci <- binom.test(sum(wins), length(wins), conf.level = 0.95)$conf.int
     summary$ymin[i] <- ci[1]
     summary$ymax[i] <- ci[2]
-    print(i / nrow(summary))
+    # print(mean(!target_hit(data[[i]][[1]], target = 1e1, strains = c("N_A", "N_B"))))
 }
-log_plot(data[[11]][[1]][data[[1]][[1]]$rep <= 10, ])
-
+final <- final_pop(data[[1]][[1]], c("N_B"))
+sort(final[final != 0])
+which(final > 0 & final < 1e2)
+summary$total_pop / summary$N2
+log_plot(data[[1]][[1]][data[[1]][[1]]$rep <= 10, ])
 # plot with one independent variable and one dependent variable
+summary$m_A_discrete <- as.factor(round(summary$m_A, 3))
 ggplot(summary, aes(x = cA, y = wins, color = m_A_discrete)) +
     geom_point(size = 3) +
     geom_errorbar(aes(ymin = ymin, ymax = ymax)) +
@@ -92,28 +191,7 @@ ggplot(summary, aes(x = cA, y = wins, color = m_A_discrete)) +
         legend.position = "bottom"
     )
 
-# plot with two independent variables and one dependent variable
-ggplot(summary, aes(x = log2(m_A), y = cA)) +
-    geom_tile(aes(fill = wins)) +
-    labs(fill = "P(extinct)") +
-    theme_minimal() +
-    labs(
-        x = "Log2 proportion of de novo mutations that are A",
-        y = "Proportion of dose that is drug A",
-        size = "Probability that MDR emerges"
-    ) +
-    scale_fill_gradient(low = "white", high = "blue", limits = c(0, 1)) +
-    theme(
-        axis.title = element_text(size = 25, face = "bold"),
-        axis.text = element_text(size = 25),
-        legend.title = element_text(size = 20),
-        legend.text = element_text(size = 20),
-        legend.position = "bottom",
-        legend.key.width = unit(2, "cm"),
-        legend.spacing.x = unit(1, "cm"),
-        strip.text = element_text(size = 25, face = "bold")
-    )
-    
+
 summary
 sol <- data[[1]][[1]][data[[1]][[1]]$rep == 43, ]
 sol2 <- sol[sol$variable =="N_S" & sol$time <= 15,]
