bbd46838410b00aa8d68d265b9db40dbc65d879c
Oscar-Delaney <o.delaney@uq.net.au>
2024-01-30 17:09:54 +1000

    add zeta and kappa figures code

--- mutation_rate_variation/figures.R
+++ mutation_rate_variation/figures.R
@@ -24,6 +24,12 @@ c = 2, kappa = 1, cost = 0, net = 0, d = 0, gap = 1e4, zeta_rand = FALSE) {
     for (i in seq_len(nrow(summary))) {
         m_A <- 1 / (1 + 1 / summary$m_ratio[i])
         c_A <- 1 / (1 + 1 / summary$c_ratio[i])
+        if ("kappa" %in% names(summary)) {
+            kappa <- summary$kappa[i]
+        }
+        if ("zeta" %in% names(summary)) {
+            zeta <- summary$zeta[i]
+        }
         data <- simulate(
             init = c(N_S = init_S, N_A = 0, N_B = 0, N_AB = 0),
             R0 = 1e10,
@@ -100,6 +106,18 @@ create_grid <- function(length = 10) {
 }
 
 # plot with two independent variables and one dependent variable
+custom_theme <- theme_minimal() + 
+    theme(
+        axis.title = element_text(size = 25, face = "bold"),
+        axis.text = element_text(size = 25),
+        legend.title = element_text(size = 20),
+        legend.text = element_text(size = 20),
+        legend.position = "bottom",
+        legend.key.width = unit(2, "cm"),
+        legend.spacing.x = unit(1, "cm"),
+        strip.text = element_text(size = 25, face = "bold")
+    )
+
 summary_plot <- function(fine, coarse, var = "extinct", theory = TRUE, pare = FALSE) {
     # Find the y-values that maximize 'var' for each x-value
     max_df <- fine
@@ -131,22 +149,12 @@ summary_plot <- function(fine, coarse, var = "extinct", theory = TRUE, pare = FA
         facet_grid(cidal_B ~ cidal_A) +
         geom_text(data = labels, aes(label = label), size = 15, fontface = "bold") +
         labs(fill = "P(extinct)") +
-        theme_minimal() +
+        custom_theme +
         labs(
             x = "Log2 odds of A-resistance:B-resistance",
             y = "Log2 odds of A-molecule:B-molecule"
         ) +
-        scale_fill_gradient(low = "white", high = "blue", limits = c(0, 1)) +
-        theme(
-            axis.title = element_text(size = 25, face = "bold"),
-            axis.text = element_text(size = 25),
-            legend.title = element_text(size = 20),
-            legend.text = element_text(size = 20),
-            legend.position = "bottom",
-            legend.key.width = unit(2, "cm"),
-            legend.spacing.x = unit(1, "cm"),
-            strip.text = element_text(size = 25, face = "bold")
-        )
+        scale_fill_gradient(low = "white", high = "blue", limits = c(0, 1))
 
     if (theory) {
         p <- p + geom_line(data = max_df, aes(x = log2(m_ratio), y = log2(c_ratio)), color = "yellow", linewidth = 3)
@@ -188,3 +196,51 @@ run_and_save("costs", args = list(cost = 0.1))
 # run_and_save("net", args = list(net = -0.1))
 run_and_save("net_kappa", args = list(net = -0.1, kappa = 3))
 run_and_save("pk", args = list(gap = 12, d = 0.15, net = -0.2), theory = FALSE)
+
+kappa_var <-   expand.grid(c_ratio = 2 ^ seq(0, 4, length.out = 200), m_ratio = 0.5,
+              kappa = 2 ^ seq(-2, 2, length.out = 50), cidal_A = c(0), cidal_B = c(0))
+
+sims <- run_sims(kappa_var, config_only = TRUE, net = -0.1)
+
+max_df <- sims %>%
+    group_by(kappa, m_ratio) %>%
+    arrange(desc(extinct)) %>%
+    slice_head(n = 1) %>%
+    ungroup()
+
+ggplot(max_df, aes(x = log2(kappa))) +
+        geom_point(aes(y = log2(c_ratio)), size = 5) +
+        labs(
+            x = "Log2 of kappa",
+            y = "Log2 odds of A-molecule:B-molecule",
+            color = "log2 mutation rate ratio"
+        ) +
+        custom_theme
+
+ggsave(filename = paste0(dir, "kappa.pdf"), width = 10, height = 10)
+
+z_var <-   expand.grid(c_ratio = seq(1, 3, length.out = 30),
+                m_ratio = 2^-3,
+              zeta = 2 ^ seq(0, 4, length.out = 20),
+              cidal_A = c(0),
+              cidal_B = c(0))
+
+sims <- run_sims(z_var, config_only = TRUE, zeta_rand = TRUE, net = 0)
+
+max_df <- sims %>%
+    group_by(zeta) %>%
+    arrange(desc(extinct)) %>%
+    slice_head(n = 1) %>%
+    ungroup()
+
+ggplot(max_df, aes(x = log2(zeta))) +
+        geom_point(aes(y = log2(c_ratio)), size = 5) +
+        xlim(c(0, 4)) +
+        ylim(c(0, 1.6)) +
+        labs(
+            x = "Log2 zeta",
+            y = "Log2 odds of A-molecule:B-molecule"
+        ) +
+        custom_theme
+
+ggsave(filename = paste0(dir, "zeta.pdf"), width = 10, height = 10)
